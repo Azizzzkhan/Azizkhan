5febeaebc50ae6b9c1b9645506b3d1c159ddc3b1
Azizzzkhan <abdulaev_azizkhan@mail.ru>
2022-06-14 00:33:54 +0300

    Init

--- src/AQSASas/AAAA.java
+++ src/AQSASas/AAAA.java
@@ -4,6 +4,8 @@ public class AAAA {
     public static void main(String[] args) {
         String a = "Pizdec";
         System.out.println(a);
+        System.out.printf("Aaaaaa");
+
 
     }
     }
---
+++ src/Azxasa.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public class Azxasa {
+    var Object = new ();
+}
